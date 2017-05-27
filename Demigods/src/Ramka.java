b477c8a17f729d8f31abb1f84e378d8afc34045a
ArekerA <Arek.ludwikowski@hotmail.com>
2017-05-27 13:42:15 +0200

    Ramka testowwa 2

--- Demigods/src/Ramka.java
+++ Demigods/src/Ramka.java
@@ -1,6 +1,8 @@
 
 
 
+import java.beans.FeatureDescriptor;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,13 +16,7 @@ public class Ramka extends JFrame{
 		 
 	    public Ramka() {
 	        super("Demigos Strife");
-	        Obraz obraz=new Obraz("img/hex.png",0,0);
-	        Obraz obraz2=new Obraz("img/hex.png",92,92);
-	        JPanel mainPanel= new JPanel();
-	        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-	        mainPanel.add(obraz);
-	        mainPanel.add(obraz2);
-        	add(mainPanel);
+        	add(new FrameWalka());
 	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        setVisible(true);
 	        setSize(800,600);
--- Demigods/src/walka/FrameWalka.java
+++ Demigods/src/walka/FrameWalka.java
@@ -1,12 +1,16 @@
 package walka;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 import interfejs.Obraz;
 
 public class FrameWalka extends JPanel {
 	public FrameWalka() {
-        Obraz nowetlo=new Obraz("img/hex.png",0,0);
-        add(nowetlo);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(new Obraz("img/hex.png",0,0));
+        add(new Obraz("img/hex.png",80,0));
+        add(new Obraz("img/hex.png",160,0));
+        add(new Obraz("img/hex.png",240,0));
     }
 }
