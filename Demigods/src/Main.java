ecf4d7e5d7382fa73bac79d65a9140799491426e
ArekerA <Arek.ludwikowski@hotmail.com>
2017-05-27 12:03:20 +0200

    stworzenie okna testowego

---
+++ Demigods/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+import interfejs.Frame;
+ 
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Frame();
+	}
+
+}
