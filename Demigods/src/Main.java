7cb895bccf058e04a60c71fa610535d2cac31017
ArekerA <Arek.ludwikowski@hotmail.com>
2017-05-31 12:22:04 +0200

    aass

--- Demigods/src/Main.java
+++ Demigods/src/Main.java
@@ -4,9 +4,7 @@ import walka.FrameWalka;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
+		// TODO Auto-generated method 
 		Ramka ramkaglowna=new Ramka();
 		RamkaKreatora walka=new RamkaKreatora();
 		
